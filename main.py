95d4e7b214075a2bea7fb365ab12b8a13f732c49
Michael French <michael.x.french@gmail.com>
2022-12-11 22:06:04 -0600

    Cron job preparation

--- main.py
+++ main.py
@@ -22,18 +22,13 @@ def add_track_to_db(track: dict):
     track_ref = db.collection(u'tracks').document(db_id)
     track_ref.set(track)
 
-while True:
-    print("Running Query...")
+def main():
     results = sp.current_user_recently_played(limit=50)
     tracks = results['items']
     
     for track in tracks:
         add_track_to_db(track)
         
-    # with open('data.json', 'w') as outfile:
-    #     json.dump(results['items'], outfile, sort_keys=True, indent=4)
     
-    old_results = results
-    last_timestamp = results['items'][0]['played_at']
-
-    time.sleep(60)
+if __name__ == "__main__":
+    main()
